ff2fd39d98911378d5bd55d7a37a252007f36b0c
Edu <eduvazher@gmail.com>
2023-12-08 23:46:03 -0600

    Update homicidios_inegi.R
    final con la limpieza de columnas

--- homicidios_inegi.R
+++ homicidios_inegi.R
@@ -1,15 +1,17 @@
 install.packages("dplyr")
 install.packages("purrr")
 install.packages("foreign") 
+install.packages("tidyverse")
 
 
 library(foreign)
 library(dplyr)
 library(purrr)
 library(stringr)
+library(tidyverse)
 
 
-carpeta <- "~/Documents/tesis_edu"
+carpeta <- "C:/Users/eduva/Documents/Tesis/dataset"
 
 # Obtener la lista de archivos dbf en la carpeta
 
@@ -39,8 +41,8 @@ defun_dataframes_name <- imap(defun_dataframes, ~ .x %>% mutate(FILE_NAME = .y))
 ###En 2022 cambio el nombre dela columna
 
 defun_dataframes_name$DEFUN22.dbf <- defun_dataframes_name$DEFUN22.dbf %>%   rename(
-                                                                             PRESUNTO = TIPO_DEFUN
-                                                                                    )
+  PRESUNTO = TIPO_DEFUN
+)
 
 defun_filtered <- lapply(defun_dataframes_name, function(df) {
   df %>%
@@ -67,11 +69,11 @@ dataframe_final_stg$ENT_OCURR <- as.numeric(dataframe_final_stg$ENT_OCURR)
 ###Sexo:  Replace 99's to not identified
 
 dataframe_final_stg$SEXO  <- case_when(
-                              dataframe_final_stg$SEXO == 1  ~ "Hombre",
-                              dataframe_final_stg$SEXO == 2  ~ "Mujer",
-                              dataframe_final_stg$SEXO == 9  ~ "No especificado",
-                              .default = "Check"
-                                  )
+    dataframe_final_stg$SEXO == 1  ~ "Hombre",
+    dataframe_final_stg$SEXO == 2  ~ "Mujer",
+    dataframe_final_stg$SEXO == 9  ~ "No especificado",
+    .default = "Check"
+  )
 
 dataframe_final_stg %>% distinct(SEXO)
 
@@ -79,16 +81,18 @@ dataframe_final_stg %>% distinct(SEXO)
 
 ###ENT_REGIS
 
-catalogo_estados <- read.csv("~/Documents/tesis_edu/catalogo_entidades.csv", header = TRUE)
+catalogo_estados <- read.csv("~/Tesis/dataset/catalogo_entidades.csv", header = TRUE)
 
 dataframe_final_stg <- left_join(dataframe_final_stg,
                                  catalogo_estados,
                                  by = c("ENT_REGIS" = "clave"))
 
 dataframe_final_stg <- dataframe_final_stg %>%
-                                            rename(
-                                            ENT_REGIS_NAME = entidad
-                                            )
+  rename(
+    ENT_REGIS_NAME = entidad
+  )
+
+dataframe_final_stg <- select(dataframe_final_stg, -indice_paz)
 
 ###ENT_OCURR
 
@@ -97,26 +101,31 @@ dataframe_final_stg <- left_join(dataframe_final_stg,
                                  by = c("ENT_OCURR" = "clave"))
 
 dataframe_final_stg <- dataframe_final_stg %>%
-                                            rename(
-                                                    ENT_OCURR_NAME = entidad
-                                                  )
+  rename(
+    ENT_OCURR_NAME = entidad
+  )
 
+dataframe_final_stg <- select(dataframe_final_stg, -indice_paz)
+
+###filtromos los homidicios
+
+dataframe_final_stg %>% distinct(PRESUNTO)
 
 dataframe_final_stg <- filter(dataframe_final_stg, PRESUNTO == 2)
 
-filter(dataframe_final_stg, ANIO_OCUR == 99)
+#filter(dataframe_final_stg, ANIO_OCUR == 99)
 
 ###Arreglar nombre de las columnas
 
 ###ANIO_OCURR
 
 dataframe_final_stg$ANIO_OCUR_FIXED  <- case_when(
-                                        str_trim(dataframe_final_stg$ANIO_OCUR)   == 99  ~ "No especificado",
-                                        str_length(dataframe_final_stg$ANIO_OCUR) == 1   ~ as.character(paste0("190",dataframe_final_stg$ANIO_OCUR)),
-                                        str_length(dataframe_final_stg$ANIO_OCUR) == 2   ~ as.character(paste0("19" ,dataframe_final_stg$ANIO_OCUR, collpase = NULL)),
-                                        str_length(dataframe_final_stg$ANIO_OCUR) == 4   ~ as.character(dataframe_final_stg$ANIO_OCUR),
-                                        .default = "Check"
-                                        )
+  str_trim(dataframe_final_stg$ANIO_OCUR)   == 99  ~ "No especificado",
+  str_length(dataframe_final_stg$ANIO_OCUR) == 1   ~ as.character(paste0("190",dataframe_final_stg$ANIO_OCUR)),
+  str_length(dataframe_final_stg$ANIO_OCUR) == 2   ~ as.character(paste0("19" ,dataframe_final_stg$ANIO_OCUR, collpase = NULL)),
+  str_length(dataframe_final_stg$ANIO_OCUR) == 4   ~ as.character(dataframe_final_stg$ANIO_OCUR),
+  .default = "Check"
+)
 
 ###EN anio_ocurr revisamos que tenemos variables no especificado (codigo 99) 
 ###y 9999 que tambien es no especficado
@@ -128,17 +137,15 @@ dataframe_final_stg$ANIO_OCUR_FIXED[dataframe_final_stg$ANIO_OCUR_FIXED == "9999
 check <- dataframe_final_stg %>% count(ANIO_OCUR_FIXED)
 check
 
-
-
 ###ANIO_REGIS CORREGIR
 
 dataframe_final_stg$ANIO_REGIS_FIXED  <- case_when(
-                                        str_trim(dataframe_final_stg$ANIO_REGIS)   == 99  ~ "No especificado",
-                                        str_length(dataframe_final_stg$ANIO_REGIS) == 1   ~ as.character(paste0("190",dataframe_final_stg$ANIO_REGIS)),
-                                        str_length(dataframe_final_stg$ANIO_REGIS) == 2   ~ as.character(paste0("19" ,dataframe_final_stg$ANIO_REGIS, collpase = NULL)),
-                                        str_length(dataframe_final_stg$ANIO_REGIS) == 4   ~ as.character(dataframe_final_stg$ANIO_REGIS),
-                                        .default = "Check"
-                                        )
+  str_trim(dataframe_final_stg$ANIO_REGIS)   == 99  ~ "No especificado",
+  str_length(dataframe_final_stg$ANIO_REGIS) == 1   ~ as.character(paste0("190",dataframe_final_stg$ANIO_REGIS)),
+  str_length(dataframe_final_stg$ANIO_REGIS) == 2   ~ as.character(paste0("19" ,dataframe_final_stg$ANIO_REGIS, collpase = NULL)),
+  str_length(dataframe_final_stg$ANIO_REGIS) == 4   ~ as.character(dataframe_final_stg$ANIO_REGIS),
+  .default = "Check"
+)
 ###Validamos
 
 check <- dataframe_final_stg %>% count(ANIO_REGIS_FIXED)
@@ -148,17 +155,17 @@ check
 ###PUEDE HABER CASOS DONDE EL AÑO DE OCURRIA ES NO ESPECIFICADO
 
 dataframe_final_stg$EDAD_FIXED  <-        case_when(
-                                                    dataframe_final_stg$EDAD   <  0  ~ "No especificado",
-                                                    dataframe_final_stg$EDAD   > 120 ~ "No especificado",
-                                                    .default = as.character(dataframe_final_stg$EDAD)
-                                                    )
+  dataframe_final_stg$EDAD   <  0  ~ "No especificado",
+  dataframe_final_stg$EDAD   > 120 ~ "No especificado",
+  .default = as.character(dataframe_final_stg$EDAD)
+)
 
 
 check_edad <- dataframe_final_stg %>% count(EDAD_FIXED)
 check_edad
 
 dataframe_final_stg$EDAD_FIXED_GROUP  <- if_else(dataframe_final_stg$EDAD_FIXED == "No especificado","No especificado", 
-        if_else(dataframe_final_stg$EDAD_FIXED < 18 , "Adulto", "No adulto" ))
+                                                 if_else(dataframe_final_stg$EDAD_FIXED < 18 , "Adulto", "No adulto" ))
 
 check_edad <- dataframe_final_stg %>% count(EDAD_FIXED_GROUP)
 check_edad
@@ -168,13 +175,106 @@ check_edad
 catalogo_estados$clave <- as.numeric(catalogo_estados$clave)
 
 dataframe_final_stg <- left_join(dataframe_final_stg,
-                                      catalogo_estados,
-                                      by = c("ENT_REGIS" = "clave"))
+                                 catalogo_estados,
+                                 by = c("ENT_REGIS" = "clave"))
 
 dataframe_final_stg <- select(dataframe_final_stg, -c(entidad))
-  
+
 check_ent_ocur <- dataframe_final_stg %>% count(ENT_OCURR)
 check_ent_ocur
+
+filter(dataframe_final_stg, ENT_OCURR == 33)
+
+##34 es para otros paises de latinoamerica
+##33 Estados Unidos de America
+
+
+###Data analysis
+###Filtros para aplicar al modelo
+###1. Variable ANIO_OCURRUR: -Filtrar todos los homocidios menos a 1990
+###                          -Filtrar lo no especificado
+###2. Mes Ocurr:             -Filtrar homocidios no especificados
+###                          -Filtrar registros en blanco
+###3. Correr script para validar si existen valores no especificados
+###4. Edad Fixed             -Filtrar registros no especificados
+###5. Indice de paz          -Borrar Nulls
+
+
+
+###-Filtrar todos los homocidios menos a 1990 y lo no especificado
+
+
+dataframe_final_stg_test <- filter(dataframe_final_stg, ANIO_OCUR_FIXED > 1989 & ANIO_OCUR_FIXED != "No especificado")
+
+dataframe_final_stg_test %>% distinct(ANIO_OCUR_FIXED) 
+
+dataframe_final_stg <- filter(dataframe_final_stg, ANIO_OCUR_FIXED > 1989 & ANIO_OCUR_FIXED != "No especificado")
+
+###Filtrar mes no especificados
+
+dataframe_final_stg %>% distinct(MES_OCURR)  ##Revisamos la data de los homocidios no especificados
+dataframe_final_stg %>% count(MES_OCURR)
+
+dataframe_final_stg_test %>% filter(dataframe_final_stg, MES_OCURR != "99")
+dataframe_final_stg %>% distinct(MES_OCURR)
+
+
+
+
+subset(dataframe_final_stg,ENT_REGIS == 16 & MUN_REGIS == 108 & 
+         ENT_OCURR == 16 & MUN_OCURR == 108 & SEXO == "Hombre,")
+
+
+dataframe_final_stg %>% distinct(ANIO_OCUR_FIXED)
+
+dataframe_final_stg <- filter(dataframe_final_stg, ANIO_OCUR_FIXED == 2)
+
+
+
+
+
+
+
+check_edad <- dataframe_final_stg %>% count(EDAD_FIXED_GROUP)
+check_edad
+
+
+###Data Analysis Numero 1
+
+
+          
+
+check <- dataframe_final_stg %>% count(ANIO_OCUR_FIXED)
+check
+
+
+
+
+#final
+
+df_hom <- dataframe_final_stg
+
+####GROUP BY datasets
+
+df_hom_grouped <-    df_hom %>%
+                        group_by(ANIO_OCUR,MES_OCURR,EDAD_FIXED_GROUP,indice_paz) %>%
+                        summarise(n = n())
+
+
+df_hom_grouped
+
+
+as.Date(paste(df_hom_grouped$ANIO_OCUR_FIXED, df_hom_grouped$MES_OCURR, "01", sep = "-"),format = "%Y-%m-%d")
+
+###Spread
+
+write.csv(df_hom, "df_hom.csv")
+
+
+test <- spread(df_hom_grouped,key=c(EDAD_FIXED_GROUP,indice_paz), value=n)
+test
+
+test1 <- spread(test1)
 
 
 
@@ -191,10 +291,10 @@ dataframe_final_stg %>%
 left_join(first_df,
           second_df %>% dplyr::select(date, elephants, cats),
           by = "date")
-  
-  left_join(dataframe_final_stg,
-                                 catalogo_estados,
-                                 by = c("ENT_REGIS" = "clave"))
+
+left_join(dataframe_final_stg,
+          catalogo_estados,
+          by = c("ENT_REGIS" = "clave"))
 
 
 
